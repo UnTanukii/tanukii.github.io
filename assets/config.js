ce561eaf991eb09891cdb0bd6992f0943866e00b
Tanukii <tiphax.pro@gmail.com>
2022-05-14 17:58:19 +0200

    v1

---
+++ assets/config.js
@@ -0,0 +1,22 @@
+const config = {
+    
+    USERNAME: "Tanukii#7115",
+    PICTURE: "assets/img/pp.jpg",
+
+    DESCRIPTION: "Hi I'm <b>Tanukii</b> ! I am a french and young discord bot developper. I code in <u>JavaScript</u> and I use the library <u>discord.js</u> for my bots.",
+
+    BADGES: [
+        { id: "HypeSquad Bravery", image: "assets/img/bravery.webp" }
+    ],
+
+    PROJECTS: [
+        { id: "pewpew", image: "assets/img/projects/pewpew.webp", link: "https://bit.ly/3suEYNW", name: "Pew Pew, a bot to <s>$#%$€*#&@</s> (soon) !" },
+        { id: "elixir", image: "assets/img/projects/elixir.webp", link: "https://discord.gg/q3B4bkM", name: "Elixir Bot, a bot to get gif or normal anime pictures !" },
+        { id: "rfs-store", image: "assets/img/projects/rfs.png", link: "https://discord.gg/2uQbUDHReE", name: "RFS Store, a FiveM and Discord Bot Store." },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/UnTanukii" },
+        { class_name: "fab fa-twitter", link: "https://twitter.com/UnTanukii" }
+    ],
+}
