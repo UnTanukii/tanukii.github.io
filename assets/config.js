32f2e0ada608ba8a558bca4d0f75bbe080a7d61f
Tanukii <tiphax.pro@gmail.com>
2022-07-06 14:19:17 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -14,7 +14,6 @@ const config = {
     PROJECTS: [
         { id: "rfs-store", image: "assets/img/projects/rfs.png", link: "https://discord.gg/2uQbUDHReE", name: "RFS Store, a FiveM and Discord Bot Store." },
         { id: "elixir", image: "assets/img/projects/elixir.webp", link: "https://dsc.gg/elixir-bot", name: "Elixir Bot, a bot to get gif or normal anime pictures !" },
-        { id: "pewpew", image: "assets/img/projects/pewpew.webp", link: "https://dsc.gg/pewpew-support", name: "Pew pew, a bot for like your favourite messages !" },
     ],
 
     SOCIALS: [
